f42495516f4e8fe96e048013dfc7db73ab9e03e4
LevAverkiev <levaverkiev@gmail.com>
2022-06-06 23:24:18 +0300

    Сделал задания 2 урока

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
-let title;
-let screens;
-let screenPrice;
-let rollback;
-let fullPrice;
-let adaptive;
+let title = 'My project';
+let screens = 'Простые, Сложные, Интерактивные';
+let screenPrice = 1000;
+let rollback = 15;
+let fullPrice = 100000;
+let adaptive = true;
 
-alert('trololo');
-
-console.log('Это я!');
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
+console.log(screens.length);
+console.log('Стоимость верстки экранов ' + screenPrice + ' рублей');
+console.log('Стоимость разработки сайта ' + fullPrice + ' рублей');
+console.log(screens.toLowerCase().split(', '));
+console.log('Процент отката посреднику за работу ' + (fullPrice * (rollback/100)));
