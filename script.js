764d4eaa92a1778b711eb5566dc0e65cf51f088a
LevAverkiev <levaverkiev@gmail.com>
2022-06-13 09:44:54 +0300

    Сделал все задания 3 урока усложненные

--- script.js
+++ script.js
@@ -1,44 +1,52 @@
-// let title = 'My project';
-let title = prompt('Как называется наш проект?', 'My project')
-// let screens = 'Простые, Сложные, Интерактивные';
-let screens = prompt('Какие типы экранов нужно разработать?', 'Простые, Сложные, Интерактивные')
-// let screenPrice = 1000;
-let screenPrice = +prompt('Сколько будет стоить данная работа?', 1000)
-let adaptive = confirm('Нужен ли адаптив на сайте?')
-let service1 = prompt('Какой дополнительный тип услуги нужен?', 'Верстка хедера')
-let servicePrice1 = +prompt('Сколько это будет стоить?', 2000)
-let service2 = prompt('Какой дополнительный тип услуги нужен?', 'Верстка футера')
-let servicePrice2 = +prompt('Сколько это будет стоить?', 3000)
-// let fullPrice = 100000;
-let fullPrice = screenPrice + servicePrice1 + servicePrice2
-let rollback = 15;
-let servicePercentPrice = fullPrice - (fullPrice * (rollback/100))
-// let adaptive = true;
+// 1 задание
 
-// console.log(typeof title);
-console.log(title);
-console.log(screens);
-console.log(screenPrice);
-console.log(adaptive);
-console.log(service1);
-console.log(servicePrice1);
-console.log(service2);
-console.log(servicePrice2);
-console.log(fullPrice);
-console.log(servicePercentPrice);
-if (fullPrice >= 30000) {
-    console.log('Даем скидку в 10%');
-} else if (fullPrice < 30000 && fullPrice >= 15000) {
-    console.log('Даем скидку в 5%');
-} else if (fullPrice < 15000 && fullPrice > 0) {
-    console.log('Скидка не предусмотрена');
-} else {
-    console.log('Что то пошло не так');
-}
-console.log(typeof fullPrice);
-console.log(typeof adaptive);
-console.log(screens.length);
-console.log('Стоимость верстки экранов ' + screenPrice + ' рублей');
-console.log('Стоимость разработки сайта ' + fullPrice + ' рублей');
-console.log(screens.toLowerCase().split(', '));
-console.log('Процент отката посреднику за работу ' + (fullPrice * (rollback/100)));
+// let lang = prompt('Какой язык выберите?(Введите ru или en)')
+
+// через if
+
+// if (lang === null) {
+//     alert('Вы нажали Отмена!')
+// } else if (lang === '') {
+//     alert('Вы нажали Продолжить без значения!')
+// } else if (lang !== 'ru' && lang !== 'en') {
+//     alert('Вы ввели неправильное значение!')
+// } else if (lang === 'ru') {
+//     alert('Понеденик, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье')
+// } else if (lang === 'en') {
+//     alert('Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday')
+// }
+
+// через switch-case
+
+// switch (true) {
+//     case lang === null:
+//         alert('Вы нажали Отмена!')
+//         break
+//     case lang === '':
+//         alert('Вы нажали Продолжить без значения!')
+//         break
+//     case lang !== 'ru' && lang !== 'en':
+//         alert('Вы ввели неправильное значение!')
+//         break
+//     case lang === 'ru':
+//         alert('Понеденик, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье')
+//         break
+//     case lang === 'en':
+//         alert('Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday')
+//         break
+// }
+
+// через многомерный массив без ифов и switch
+
+// let langs = {
+//     ru: ['Понеденик', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'],
+//     en: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+// }
+
+// alert(langs[lang])
+
+// 2 задание
+
+let namePerson = prompt('Введите имя');
+
+namePerson === 'Артем' ? console.log('директор') : namePerson === 'Александр' ? console.log('преподаватель') : console.log('студент')
