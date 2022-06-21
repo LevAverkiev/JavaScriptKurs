6ab55d1d07360b15ab62255cf449f0c0fa1b48cf
LevAverkiev <levaverkiev@gmail.com>
2022-06-21 08:09:11 +0300

    Сделал задания 4 урока усложненка

--- script.js
+++ script.js
@@ -1,61 +1,17 @@
 'use strict'
 
-let title = prompt('Как называется наш проект?', 'My project')
-let screens = prompt('Какие типы экранов нужно разработать?', 'Простые, Сложные, Интерактивные')
-let screenPrice = +prompt('Сколько будет стоить данная работа?', 1000)
-let adaptive = confirm('Нужен ли адаптив на сайте?')
-let service1 = prompt('Какой дополнительный тип услуги нужен?', 'Верстка хедера')
-let servicePrice1 = +prompt('Сколько это будет стоить?', 2000)
-let service2 = prompt('Какой дополнительный тип услуги нужен?', 'Верстка футера')
-let servicePrice2 = +prompt('Сколько это будет стоить?', 3000)
-let allServicePrices
-let fullPrice
-let servicePercentPrice
-let rollback = 15;
-
-const showTypeOf = function(variable) {
-    console.log(variable, typeof variable);
-}
-
-const getRollBackMessage = function(price) {
-    if (price >= 30000) {
-        return 'Даем скидку в 10%'
-    } else if (price < 30000 && price >= 15000) {
-        return 'Даем скидку в 5%'
-    } else if (price < 15000 && price > 0) {
-        return 'Скидка не предусмотрена'
+const findFunc = function(item) {
+    if (typeof(item) != 'string') { //проверка на тип 'строка'
+        console.log('Это не строка'); 
     } else {
-        return 'Что то пошло не так'
+        item = item.trim() //убрали пробелы в начале и в конце строки
+        if (item.length > 30) { //проверка на кол-во символов
+            let str = item.slice(0, 30) //оставить строку с 1 по 30 символ. Остальное обрезать
+            console.log(str + '...'); //вывести строку + ... в конце
+        } else {
+            console.log(item);
+        }
     }
 }
 
-const getAllServicePrices = function () {
-    return servicePrice1 + servicePrice2
-}
-
-function getFullPrice() {
-    return screenPrice + allServicePrices
-}
-
-const getTitle = function () {
-    title = title.trimStart()
-    return title[0].toUpperCase() + title.substring(1)
-}
-
-const getServicePercentPrices = function () {
-    return fullPrice - (fullPrice * (rollback / 100))
-}
-
-allServicePrices = getAllServicePrices()
-fullPrice = getFullPrice()
-servicePercentPrice = getServicePercentPrices()
-
-console.log(allServicePrices);
-console.log(fullPrice);
-console.log(getTitle());
-showTypeOf(title);
-showTypeOf(fullPrice);
-showTypeOf(adaptive);
-console.log(screens.toLowerCase().split(', '));
-console.log(servicePercentPrice);
-console.log(getRollBackMessage(fullPrice));
+findFunc('   1    gfdgdfgfdgfdgfdgergdfcbxftbterebvcbdfb');
